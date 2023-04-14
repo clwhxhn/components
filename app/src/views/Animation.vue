8566209c94dc0351c279b5957e9b08f319eeb92e
wangxiaohui <clwhxhn@163.com>
2023-04-14 14:47:06 +0800

    11

---
+++ app/src/views/Animation.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div>
+    <img src="@/assets/images/led/icon-location.png" alt="" class="quality-img" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+@import "~@/assets/less/torem.less";
+.quality-img {
+  cursor: pointer;
+  .px2rem(width, 88);
+  .px2rem(height, 146);
+  animation: bounce-down 1.6s linear infinite;
+}
+@keyframes bounce-down {
+  25% {
+    transform: translateY(-0.2rem);
+  }
+  50%,
+  100% {
+    transform: translateY(0);
+  }
+  75% {
+    transform: translateY(0.5rem);
+  }
+}
+</style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -2,45 +2,58 @@
   <div class="container">
     <!-- 菜单 -->
     <ul class="menu">
-        <li @click="key = 0">
-            滚动的表格
-        </li>
+      <li v-for="(item, index) in list" :key="item.key" @click="key = item.key">
+        {{ item.text }}
+      </li>
     </ul>
     <div class="content">
-        <!-- 滚动的表格 -->
-        <Table v-if="key === 0"></Table>
+      <!-- 滚动的表格 -->
+      <Table v-if="key === 0"></Table>
+      <PdImage v-if="key === 1"></PdImage>
     </div>
   </div>
 </template>
 
 <script>
-import Table from './Table.vue'
+import Table from "./Table.vue";
+import PdImage from './Animation.vue'
 export default {
   name: "Home",
   components: {
     Table,
+    PdImage
   },
   data() {
     return {
-        key: -1
+      key: -1,
     };
   },
   created() {
+    this.list = [
+      {
+        key: 0,
+        text: "滚动的表格",
+      },
+      {
+        key: 1,
+        text: '上下飘动的图片'
+      },
+    ];
   },
 };
 </script>
 
 <style lang="less" scoped>
 .container {
-    display: flex;
-    .menu {
-        width: 150px;
-        li{
-            cursor: pointer;
-        }
+  display: flex;
+  .menu {
+    width: 150px;
+    li {
+      cursor: pointer;
     }
-    .content{
-        flex: 1;
-    }
+  }
+  .content {
+    flex: 1;
+  }
 }
 </style>
