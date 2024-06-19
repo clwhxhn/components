a4c1c6e7dbe5b5d5dd7a9aecf02eaaae071a4ebf
wangxiaohui <clwhxhn@163.com>
2024-06-19 11:34:43 +0800

    走马灯

---
+++ app/src/components/TrottingLamp.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="wrap">
+    <div class="notice-card-wrapper">
+      <div>
+        <div class="inner-container" ref="notice">
+          {{ notice }}
+          {{ "" }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      notice:
+        "2023年09月29日17时02分发布暴雨蓝色预警信号：预计未来12小时内，沙河乡及周边地区降水可达50毫米以上，请注意防范。",
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.notice-card-wrapper {
+  width: 200px;
+  height: 24px;
+  margin-bottom: 9px;
+  display: flex;
+  border-radius: 2px;
+  overflow-x: hidden;
+  img {
+    position: relative;
+    z-index: 20;
+  }
+  & > div {
+    flex: 1;
+    background: rgba(31, 103, 250, 0.08);
+    font-size: 12px;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    color: #0b80ff;
+    line-height: 17px;
+    position: relative;
+    z-index: 10;
+    .inner-container {
+      margin-left: 160%; // 把文字弄出可见区域
+      min-width: 300%;
+      animation: myMove 30s linear infinite; // 重点，定义动画
+      animation-fill-mode: forwards;
+      position: relative;
+      top: 4px;
+      padding: 0 4px;
+      // white-space: nowrap;
+    }
+  }
+}
+
+@keyframes myMove {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-160%);
+  }
+}
+</style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -2,7 +2,11 @@
   <div class="container">
     <!-- 菜单 -->
     <ul class="menu">
-      <li v-for="(item, index) in list" :key="item.key" @click="key = item.component">
+      <li
+        v-for="(item, index) in list"
+        :key="item.key"
+        @click="key = item.component"
+      >
         {{ item.text }}
       </li>
     </ul>
@@ -14,33 +18,39 @@
 
 <script>
 import Table from "./Table.vue";
-import PdImage from './Animation.vue'
+import PdImage from "./Animation.vue";
 import Upload from "./Upload.vue";
+import TrottingLamp from '@/components/TrottingLamp'
 export default {
   name: "Home",
   components: {
     Table,
     PdImage,
-    Upload
+    Upload,
+    TrottingLamp
   },
   data() {
     return {
-      key: 'Table',
+      key: "Table",
     };
   },
   created() {
     this.list = [
       {
         text: "滚动的表格",
-        component: 'Table'
+        component: "Table",
       },
       {
-        text: '图片动画',
-        component: 'PdImage'
+        text: "图片动画",
+        component: "PdImage",
       },
       {
-        text: '附件上传：antdv',
-        component: 'Upload'
+        text: "附件上传：antdv",
+        component: "Upload",
+      },
+      {
+        text: "走马灯-文字",
+        component: "TrottingLamp",
       },
     ];
   },
