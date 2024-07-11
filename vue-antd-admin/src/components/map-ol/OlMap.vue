982c6ae41e95f923069707e4821e5f09fba9bd98
wangxiaohui <clwhxhn@163.com>
2024-07-11 11:16:43 +0800

    update vue-antd-admin

---
+++ vue-antd-admin/src/components/map-ol/OlMap.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="map-layout">
+    <div id="mainMap" class="ol-map"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.map-layout {
+  width: 100%;
+  height: 100%;
+
+  .ol-map {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
---
+++ vue-antd-admin/src/pages/map-ol/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="map-wrap">
+        <OlMap></OlMap>
+    </div>
+</template>
+
+<script>
+    import OlMap from '@/components/map-ol/OlMap'
+    export default {
+        components: {
+            OlMap
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+.map-wrap {
+    height: 70vh;
+}
+</style>
