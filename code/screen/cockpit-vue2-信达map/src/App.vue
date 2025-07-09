aba6ee5791f26e633d741f29fb6a33ec2449a6d8
wangxiaohui <clwhxhn@163.com>
2025-07-09 14:10:16 +0800

    Update App.vue

--- code/screen/cockpit-vue2-信达map/src/App.vue
+++ code/screen/cockpit-vue2-信达map/src/App.vue
@@ -29,47 +29,46 @@ export default {
     };
   },
   mounted() {
-    return 
     console.log(1111111);
-    this.isLogin = false;
+    // this.isLogin = false;
 
-    const url = new URL(window.location.href);
-    const params = new URLSearchParams(url.search);
-    let refresh_token = params.get("refresh_token");
-    // query不存在时尝试从hash中查找
-    if (!refresh_token) {
-      const hash = window.location.hash;
-      // 提取refresh_token参数（假设格式为#/login?refresh_token=xxx）
-      const params = new URLSearchParams(hash.slice(1).split("?")[1]);
-      refresh_token = params.get("refresh_token");
-    }
-    if (refresh_token) {
-      // 进行token转换
-      postAction("/auth/login", {
-        grant_type: "refresh_token",
-        refresh_token: refresh_token,
-      }).then((res) => {
-        if (res.success) {
-          setToken(res.result.access_token);
-          setRefreshToken(res.result.refresh_token);
+    // const url = new URL(window.location.href);
+    // const params = new URLSearchParams(url.search);
+    // let refresh_token = params.get("refresh_token");
+    // // query不存在时尝试从hash中查找
+    // if (!refresh_token) {
+    //   const hash = window.location.hash;
+    //   // 提取refresh_token参数（假设格式为#/login?refresh_token=xxx）
+    //   const params = new URLSearchParams(hash.slice(1).split("?")[1]);
+    //   refresh_token = params.get("refresh_token");
+    // }
+    // if (refresh_token) {
+    //   // 进行token转换
+    //   postAction("/auth/login", {
+    //     grant_type: "refresh_token",
+    //     refresh_token: refresh_token,
+    //   }).then((res) => {
+    //     if (res.success) {
+    //       setToken(res.result.access_token);
+    //       setRefreshToken(res.result.refresh_token);
 
-          requestWithCache({
-            url: "/rws/rws/rwsWaterSupplyProject/getProjectInfoStat",
-          });
-          requestWithCache({
-            url: "/pcm/pcm.map/queryWrEngineeringMap",
-          });
+    //       requestWithCache({
+    //         url: "/rws/rws/rwsWaterSupplyProject/getProjectInfoStat",
+    //       });
+    //       requestWithCache({
+    //         url: "/pcm/pcm.map/queryWrEngineeringMap",
+    //       });
 
-          this.getDictList().then(() => {
-            this.isLogin = true;
-          });
-        } else {
-          this.isLogin = false;
-        }
-      });
-    } else {
-      this.isLogin = false;
-    }
+    //       this.getDictList().then(() => {
+    //         this.isLogin = true;
+    //       });
+    //     } else {
+    //       this.isLogin = false;
+    //     }
+    //   });
+    // } else {
+    //   this.isLogin = false;
+    // }
   },
   methods: {
     /**
