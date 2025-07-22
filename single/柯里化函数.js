fa128bc4065a6e7c477581487a3cc812d845ceb6
wangxiaohui <clwhxhn@163.com>
2025-07-22 16:32:31 +0800

    1

--- single/柯里化函数.js
+++ single/柯里化函数.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: wangxiaohui clwhxhn@163.com
+ * @Date: 2024-07-16 10:02:54
+ * @LastEditors: wangxiaohui clwhxhn@163.com
+ * @LastEditTime: 2025-07-22 16:32:18
+ * @FilePath: \components\single\柯里化函数.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 function add(x,y,z){
     return x+y+z
+    
 }
 
 // 封装
